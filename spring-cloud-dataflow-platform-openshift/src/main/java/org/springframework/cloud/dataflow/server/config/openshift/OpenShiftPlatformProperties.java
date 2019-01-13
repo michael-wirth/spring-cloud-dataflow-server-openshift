a1ca33b3b2d8c6d43fe6033f169bebcaf9d45e19
Michael Wirth <michael.wirth@mimacom.com>
2019-01-13 20:47:07 +0100

    fix(build): enable spring boot java format and checkstyle

--- spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftPlatformProperties.java
+++ spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftPlatformProperties.java
@@ -1,12 +1,31 @@
-package org.springframework.cloud.dataflow.server.config.openshift;
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.deployer.spi.openshift.OpenShiftDeployerProperties;
+package org.springframework.cloud.dataflow.server.config.openshift;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.deployer.spi.openshift.OpenShiftDeployerProperties;
+
 /**
+ * {@link ConfigurationProperties} class to configure various settings of Data Flow
+ * running on Openshift.
+ *
  * @author Michael Wirth
  */
 @ConfigurationProperties("spring.cloud.dataflow.task.platform.openshift")
@@ -15,7 +34,7 @@ public class OpenShiftPlatformProperties {
 	private Map<String, OpenShiftDeployerProperties> accounts = new LinkedHashMap<>();
 
 	public Map<String, OpenShiftDeployerProperties> getAccounts() {
-		return accounts;
+		return this.accounts;
 	}
 
 	public void setAccounts(Map<String, OpenShiftDeployerProperties> accounts) {
--- spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftTaskPlatformAutoConfiguration.java
+++ spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftTaskPlatformAutoConfiguration.java
@@ -1,14 +1,34 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.cloud.dataflow.server.config.openshift;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.dataflow.core.Launcher;
 import org.springframework.cloud.dataflow.core.TaskPlatform;
 import org.springframework.cloud.deployer.autoconfigure.DelegatingResourceLoaderBuilderCustomizer;
 import org.springframework.cloud.deployer.resource.docker.DockerResourceLoader;
 import org.springframework.cloud.deployer.resource.maven.MavenProperties;
-import org.springframework.cloud.deployer.spi.kubernetes.ContainerFactory;
 import org.springframework.cloud.deployer.spi.openshift.OpenShiftDeployerProperties;
 import org.springframework.cloud.deployer.spi.openshift.OpenShiftTaskLauncher;
 import org.springframework.cloud.deployer.spi.openshift.ResourceAwareOpenShiftTaskLauncher;
@@ -21,11 +41,10 @@ import org.springframework.cloud.deployer.spi.task.TaskLauncher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 /**
+ * Configures the Spring Cloud TaskPlatform based on feature toggle settings and ifrunning on
+ * Openshift.
+ *
  * @author Michael Wirth
  */
 @Configuration
@@ -78,7 +97,7 @@ public class OpenShiftTaskPlatformAutoConfiguration {
 	@Bean
 	public DelegatingResourceLoaderBuilderCustomizer dockerDelegatingResourceLoaderBuilderCustomizer(
 			MavenProperties mavenProperties) {
-		return customizer -> customizer.loader("docker", new DockerResourceLoader());
+		return (customizer) -> customizer.loader("docker", new DockerResourceLoader());
 	}
 
 }
