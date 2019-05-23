1758d0b688b92f83e919ca35f0f2ab499a3285ea
Michael Wirth <michael.wirth@mimacom.com>
2019-05-23 21:38:05 +0200

    fix: update to release versions

--- spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftTaskPlatformAutoConfiguration.java
+++ spring-cloud-dataflow-platform-openshift/src/main/java/org/springframework/cloud/dataflow/server/config/openshift/OpenShiftTaskPlatformAutoConfiguration.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -42,8 +42,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Configures the Spring Cloud TaskPlatform based on feature toggle settings and ifrunning on
- * Openshift.
+ * Configures the Spring Cloud TaskPlatform based on feature toggle settings and ifrunning
+ * on Openshift.
  *
  * @author Michael Wirth
  */
